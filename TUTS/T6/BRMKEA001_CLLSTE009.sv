303bf0592c55301a351f13f0ed4262628a2fd063
Kealym <BRMKEA001@myuct.ac.za>
2021-10-22 20:29:17 +0200

    fixed commets

--- TUTS/T6/BRMKEA001_CLLSTE009.sv
+++ TUTS/T6/BRMKEA001_CLLSTE009.sv
@@ -3,8 +3,8 @@
 
 module reg_mem (addr, data_in, wen, clk, data_out);
 
-  parameter DATA_WIDTH = 8; //4 bit wide data
-  parameter ADDR_BITS = 5; //8 Addresses
+  parameter DATA_WIDTH = 8; //8 bit wide data
+  parameter ADDR_BITS = 5; //32 Addresses
 
   input [ADDR_BITS-1:0] addr;
   input [DATA_WIDTH-1:0] data_in;
@@ -14,7 +14,7 @@ module reg_mem (addr, data_in, wen, clk, data_out);
 
   reg [DATA_WIDTH-1:0] data_out;
 
-  //8 memory locations each storing a 4bits wide value
+  //32 memory locations each storing a 8bits wide value
   reg [DATA_WIDTH-1:0] mem_array [(2**ADDR_BITS)-1:0];
 
   always @(posedge clk) begin
